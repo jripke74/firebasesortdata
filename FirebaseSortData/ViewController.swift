0ce202401689346d0161aba75da92bb71e1db3c0
Jeff Ripke <jripke74@gmail.com>
2017-06-04 08:16:20 -0700

    first commit

---
+++ FirebaseSortData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseSortData
+//
+//  Created by Jeff Ripke on 6/4/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
